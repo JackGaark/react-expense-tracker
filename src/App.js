32020caf10992506507e4478c92a8beae85b384e
Jack G <jackg@Jacks-MacBook-Air.local>
2020-11-26 22:23:37 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from 'reactstrap';
 
-function App() {
+import { ExpenseContextProvider } from './GlobalState';
+
+import Header from './components/Header';
+import Form from './components/Form';
+import List from './components/List';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ExpenseContextProvider>
+      <Container className="text-center">
+        <Header />
+        <Form />
+        <List />
+      </Container>
+    </ExpenseContextProvider>
   );
 }
-
-export default App;
